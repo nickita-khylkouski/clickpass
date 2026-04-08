34d34a139b220b498addace75225c6d3fc4d97e0
Nickita Khylkouski <90287684+nickita-khylkouski@users.noreply.github.com>
2026-04-07 23:59:56 -0700

    Remove CenterWord app from clickpass

--- apps/centerword/Sources/CenterWordApp/TeleprompterText.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct TeleprompterText {
-    static func words(in text: String) -> [String] {
-        text
-            .split(whereSeparator: \.isWhitespace)
-            .map(String.init)
-    }
-}
